7eba7d5f4a28065f30d638945ee8ef2ad1d27aa1
Adam <medaman@gmail.com>
2017-09-27 17:20:39 -0700

    Some bug fixing

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,9 @@
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE DATABASE burgers_db;
-
-USE burgers_db;
-
 CREATE TABLE burgers (
 
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(128) NOT NULL,
   devoured BOOLEAN NOT NULL,
-  date TIMESTAMP NOT NULL
+  createdAt TIMESTAMP NOT NULL,
+  PRIMARY KEY(id)
 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-USE burgers_db;
-
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Cheeseburger", false),
 	   ("Hamburger", false),
